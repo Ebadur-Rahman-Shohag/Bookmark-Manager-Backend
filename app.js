89d9ee191998a9768630c996259902643847159b
Ebadur Rahman <ebadur.rahman@sjinnovation.com>
2025-07-29 14:06:45 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const app = express();
+const Bookmark = require("./models/Bookmark");
+const connectDB = require("./db/connect");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/bookmarks", async (req, res) => {
+    const data = await Bookmark.find();
+    res.json(data);
+});
+
+
+app.post("/bookmarks", async (req, res) => {
+    const data = await req.body;
+    await Bookmark.create(data);
+    res.json(data);
+});
+
+app.delete("/bookmarks/:id", async (req, res) => {
+    const id = req.params.id;
+    await Bookmark.findByIdAndDelete(id);
+    res.json({ id });
+})
+
+const url = process.env.MONGO_URI;
+const port = process.env.PORT || 5000;
+const start = async (url) => {
+    try {
+        await connectDB(url);
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+start(url);
