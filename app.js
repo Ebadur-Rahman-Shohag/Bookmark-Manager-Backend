ea2422e7553e4d70a3cf12c7df0bcc8d206e0fda
Ebadur Rahman <ebadur.rahman@sjinnovation.com>
2025-07-30 11:52:46 +0600

    add update logic

--- app.js
+++ app.js
@@ -9,22 +9,32 @@ const connectDB = require("./db/connect");
 app.use(cors());
 app.use(express.json());
 
+// Get all the bookmark
 app.get("/bookmarks", async (req, res) => {
     const data = await Bookmark.find();
     res.json(data);
 });
 
-
+// Create a bookmark
 app.post("/bookmarks", async (req, res) => {
     const data = await req.body;
     await Bookmark.create(data);
     res.json(data);
 });
 
+// Delete a bookmark
 app.delete("/bookmarks/:id", async (req, res) => {
     const id = req.params.id;
     await Bookmark.findByIdAndDelete(id);
     res.json({ id });
+})
+
+// Update a bookmark
+app.update("/bookmarks/:id", async (req, res) => {
+    const id = req.params.id;
+    const data = await req.body;
+    await Bookmark.findByIdAndUpdate(id, data);
+    res.json(data);
 })
 
 const url = process.env.MONGO_URI;
